a91f6fedc28fe8ec6f78299a967f36a38a6e7d8f
hw0404 <110794254+hw0404@users.noreply.github.com>
2022-08-08 11:34:36 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.cc
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -1,3 +1,70 @@
+////베스트셀러
+////2022 7 8
+//
+//#include<iostream>
+//#include<map>
+//#include<string>
+//#include<vector>
+//#include<algorithm>
+//
+//
+//using namespace std;
+//#define MAX_N 1001
+//map<string,int> m;
+//vector<string> v[MAX_N];
+//int cnt[MAX_N];
+//int main(){
+//    int N;
+//    cin >> N;
+//
+//     int max = 0;
+//    for(int i=0;i<N;i++){
+//        string s;
+//        cin >> s;
+//        if(N==1){
+//            cout<< s;
+//            break;
+//        }
+//        if(i>0){
+//            if(m.find(s)==m.end()){
+//                m.insert({s,i});
+//                cnt[i]+=1;
+//                if(cnt[i]>max){
+//                    max = cnt[i];
+//                }
+//            }else{
+//                int n= m.find(s)->second;
+//                cnt[n]+=1;
+//                if(cnt[n]>max){
+//                    max = cnt[n];
+//                }
+//            }
+//        }
+//        if(i==0){
+//            m.insert({s,i});
+//            cnt[i]+=1;
+//        }
+//
+//    }
+//    if(N!=1){
+//        vector<string> v;
+//        for(auto iter = m.begin();iter!=m.end();iter++){
+//            if(cnt[iter->second] ==max){
+//                v.push_back(iter->first);
+//            }
+//        }
+//        if(v.size()==1){
+//            cout<<v[0];
+//        }else{
+//            sort(v.begin(),v.end());
+//            cout<<v[0];
+//        }
+//    }
+//    return 0;
+//}
+//
+//
+//
 #include<iostream>
 #include<map>
 #include<string>
@@ -13,49 +80,22 @@ int cnt[MAX_N];
 int main(){
     int N;
     cin >> N;
-    
-     int max = 0;
+    int Max =0;
     for(int i=0;i<N;i++){
         string s;
         cin >> s;
-        if(N==1){
-            cout<< s;
-            break;
-        }
-        if(i>0){
-            if(m.find(s)==m.end()){
-                m.insert({s,i});
-                cnt[i]+=1;
-                if(cnt[i]>max){
-                    max = cnt[i];
-                }
-            }else{
-                int n= m.find(s)->second;
-                cnt[n]+=1;
-                if(cnt[n]>max){
-                    max = cnt[n];
-                }
-            }
-        }
-        if(i==0){
-            m.insert({s,i});
-            cnt[i]+=1;
-        }
-    
+        m[s]++;
     }
-    if(N!=1){
-        vector<string> v;
-        for(auto iter = m.begin();iter!=m.end();iter++){
-            if(cnt[iter->second] ==max){
-                v.push_back(iter->first);
-            }
-        }
-        if(v.size()==1){
-            cout<<v[0];
-        }else{
-            sort(v.begin(),v.end());
-            cout<<v[0];
+    for(auto iter = m.begin();iter!=m.end();iter++){
+        Max = max(Max,iter->second);
+    }
+    for(auto iter = m.begin();iter!=m.end();iter++){
+        if(Max == (*iter).second){
+            cout<<(*iter).first;
+            return 0;
         }
     }
+
     return 0;
 }
+
