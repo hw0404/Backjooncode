61f2af58850b826278ce9f33df464e5931378845
hw0404 <110794254+hw0404@users.noreply.github.com>
2022-09-22 01:08:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -2,17 +2,15 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<cstring>
 #define MAX_N 21
 
 using namespace std;
 
 int N,M;
 int result[MAX_N];
-int visited[MAX_N];
 int num[MAX_N];
 vector<int> v;
-int cycle;
+
 int sum;
 int total;
 void input(){
@@ -27,17 +25,13 @@ void input(){
 }
 
 void solve( int cnt ){
-    if(cnt == cycle ){
-      //  for(int i=0;i<cnt;i++) cout<<num[i]<<" " ;
-        if(sum == M){
-             total+=1;
-          }
+    if(cnt == N ){
         return ;
     }
     for(int i=0;i<v.size();i++){
-        if(visited[i]==0){
             if(cnt>0){
                 if(num[cnt-1]<i){
+                    if(sum+v[i]==M) total++;
                     num[cnt]=i;
                     sum+=v[i];
                     solve(cnt+1);
@@ -45,27 +39,22 @@ void solve( int cnt ){
                 }
             }
             else{
-                sum+=v[i];
+                if(sum+v[i]==M) total++;
                 num[cnt]=i;
+                sum+=v[i];
                 solve(cnt+1);
                 sum-=v[i];
             }
         }
-    }
 }
 void solution(){
     input();
-    for(int i=1;i<=v.size();i++){
-        cycle=i;
-        sum=0;
-        memset(visited,0,sizeof(visited));
-        solve(0);
-
-    }
+    solve(0);
     cout<<total<<"\n";
-    
+
 }
 int main(){
     solution();
 return 0;
 }
+
